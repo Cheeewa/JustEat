7bf487bc5f076695617f6130cd0b24ba0148c197
mintravadee <jmintravadee@gmail.com>
2024-06-28 17:26:02 +0200

    #

--- src/View.elm
+++ src/View.elm
@@ -12,7 +12,7 @@ view model =
     div []
         [ titleSection
         ,input [ placeholder "Enter ingredients", value model.ingredients, onInput UpdateIngredients ] []
-        , button [ class "button is-primary", class "has-text-centered", onClick FetchRecipes ] [ text "Get Recipes" ]
+        , button [ class "button is-primary" ] [ text "Get Recipes" ]
         , ul [class "has-text-centered"] (List.map recipeItem model.recipes)
         ]
 
