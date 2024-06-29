9e35a490b1daf3487a67352a5700c7647d31b414
mintravadee <jmintravadee@gmail.com>
2024-06-29 09:32:18 +0200

    ..

--- src/View.elm
+++ src/View.elm
@@ -1,6 +1,6 @@
 module View exposing (view)
 
-import Html exposing (Html, div, input, button, text, ul, li, img, a,h1, h2,section)
+import Html exposing (Html, div, input, button, text, ul, li, img, a,h2)
 import Html.Attributes exposing (href, placeholder, value, src ,class)
 import Html.Events exposing (onClick, onInput)
 import Model exposing (Model, Recipe)
@@ -9,8 +9,7 @@ import Msg exposing (Msg(..))
 view : Model -> Html Msg
 view model =
     div []
-        [ h1[class "title"][text "WELCOME TO JUSTEAT"]
-        , input [ placeholder "Enter ingredients", value model.ingredients, onInput UpdateIngredients ] []
+        [ input [ placeholder "Enter ingredients", value model.ingredients, onInput UpdateIngredients ] []
         , button [ class "button is-primary", onClick FetchRecipes ] [ text "Get Recipes" ]
         , case model.selectedRecipe of
             Nothing ->
