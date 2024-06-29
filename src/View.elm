ae27b4dfa60fe814d60d85c6ca18ec350c544392
mintravadee <jmintravadee@gmail.com>
2024-06-29 10:02:11 +0200

    ..

--- src/View.elm
+++ src/View.elm
@@ -8,7 +8,7 @@ import Msg exposing (Msg(..))
 
 view : Model -> Html Msg
 view model =
-    div []
+    div [class "has-text-centered"]
         [ input [ placeholder "Enter ingredients", value model.ingredients, onInput UpdateIngredients ] []
         , button [ class "button is-primary", onClick FetchRecipes ] [ text "Get Recipes" ]
         , case model.selectedRecipe of
